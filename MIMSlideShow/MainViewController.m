1e1fd2334ba12d274a1ede4700b557f96b4b02bd
Reetu Raj <reetu.raj@gmail.com>
2011-06-26 20:33:44 +0530

    Test Commit

--- MIMSlideShow/MainViewController.m
+++ MIMSlideShow/MainViewController.m
@@ -39,7 +39,7 @@
 {
    
    
-    
+    //You can define any rectangle of any size where you want to display your slideshow.
     MIMSlideShowTemplate1 *slide=[[MIMSlideShowTemplate1 alloc]initWithFrame:CGRectMake(0, 0, 768, 1004)];
     [slide setImageArray:[NSArray arrayWithObjects:@"1.jpg",@"2.jpg",@"3.jpg",@"4.jpg",@"5.jpg",@"6.jpg",@"7.jpg",@"8.jpg",@"9.jpg",@"10.jpg",@"11.jpg",@"12.jpg",@"13.jpg",@"14.jpg",@"15.jpg",@"16.jpg", nil]];
     [slide setAnimationType:Bubbles];//
